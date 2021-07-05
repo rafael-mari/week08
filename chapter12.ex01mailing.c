77612d8fd789007a08ccbfe721eeff6e37727710
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-07-04 19:38:04 -0500

    Update chapter12.ex01mailing.c

--- chapter12.ex01mailing.c
+++ chapter12.ex01mailing.c
@@ -16,44 +16,44 @@ int main()
 	int people; // counter of people
 
 	struct list {
-		char email[30];
-		char name[50];
-		char gender[20];
-    char major[50];
-    char number[40];
-    //The structure of the list of data
+	char email[30];
+	char name[50];
+	char gender[20];
+    	char major[50];
+    	char number[40];
+    	//The structure of the list of data
 	};
-  //We enter the data of the members of the list
+  	//We enter the data of the members of the list
 	struct list receiver[] = {
 		{
-			"2009083@upy.edu.mx",
-			"Rafael Marí Reyna",
-			"Male",
-			"Computational Robotics Engineering",
-      "9992304870",
+		"2009083@upy.edu.mx",
+		"Rafael Marí Reyna",
+		"Male",
+		"Computational Robotics Engineering",
+      		"9992304870",
 		},
 		{
-			"2009107@upy.edu.mx",
-			"Jesus Irael Prado Pineda",
-			"Male",
-			"Computational Robotics Engineering",
-      "9991198737",
+		"2009107@upy.edu.mx",
+		"Jesus Irael Prado Pineda",
+		"Male",
+		"Computational Robotics Engineering",
+      		"9991198737",
 		},
-    {
-			"2009084@upy.edu.mx",
-			"Pablo Iván Martín Enriquez",
-			"Male",
-			"Computational Robotics Engineering",
-      "9994753579",
+    		{
+		"2009084@upy.edu.mx",
+		"Pablo Iván Martín Enriquez",
+		"Male",
+		"Computational Robotics Engineering",
+     		"9994753579",
 		},
-    {
-      "2009138@upy.edu.mx",
-      "Cesia Esther Tzuc Alonzo",
-      "Female",
-      "Embedded Systems Engineering",
-      "9991678534",
-    }
-	};
+    		{
+		"2009138@upy.edu.mx",
+		"Cesia Esther Tzuc Alonzo",
+		"Female",
+		"Embedded Systems Engineering",
+		"9991678534",
+ 		}
+		};
 
 	size = sizeof(receiver) / sizeof //The number of persons that are in the mailing list
   (receiver[0]);
